e4fc2cab826d66ad7c66b2179bc8f364ab658a3e
zhuxianfei <zhuxianfei@sinopharmholding-fl.com>
2020-05-15 16:14:49 +0800

    init

---
+++ src/main/java/demo01.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+/**
+ * 合并有序数组
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组
+ */
+
+public class demo01 {
+
+    /**
+     * 输入:
+     * nums1 = [1,2,3,0,0,0], m = 3
+     * nums2 = [2,5,6],       n = 3
+     *
+     * 输出: [1,2,2,3,5,6]
+     */
+
+
+//    方法一 : 合并后排序
+//
+//    最朴素的解法就是将两个数组合并之后再排序。该算法只需要一行(Java是2行)，时间复杂度较差，为O((n + m)\log(n + m))O((n+m)log(n+m))。这是由于这种方法没有利用两个数组本身已经有序这一点。
+    public int[] merge01(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+        return nums1;
+
+    }
+
+    /**
+     *  双指针 / 从后往前
+     * 方法二已经取得了最优的时间复杂度O(n + m)O(n+m)，但需要使用额外空间。这是由于在从头改变nums1的值时，需要把nums1中的元素存放在其他位置
+     */
+
+    public int[] merge02(int[] nums1, int m, int[] nums2, int n) {
+        // two get pointers for nums1 and nums2
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // set pointer for nums1
+        int p = m + n - 1;
+
+        // while there are still elements to compare
+        while ((p1 >= 0) && (p2 >= 0))
+            // compare two elements from nums1 and nums2
+            // and add the largest one in nums1
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+        // add missing elements from nums2
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+
+        return nums1;
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+        int m = 3;
+        int n = 3;
+        demo01 d = new demo01();
+        //方法一
+        int[] array = d.merge01(nums1,m,nums2,n);
+        System.out.println(Arrays.toString(array));
+
+        //方法二
+        int[] array2 = d.merge02(nums1,m,nums2,n);
+        System.out.println(Arrays.toString(array2));
+
+
+
+
+
+    }
+}
