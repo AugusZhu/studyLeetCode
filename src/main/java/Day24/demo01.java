50ef65ebf9093472eb9486a4c2247918354a0413
zhuxianfei <zhuxianfei@sinopharmholding-fl.com>
2020-07-13 20:02:17 +0800

    两个数组的交集_Day24

---
+++ src/main/java/Day24/demo01.java
@@ -0,0 +1,73 @@
+package Day24;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: ZhuXianfei
+ * @Date: 2020/7/13 17:05
+ * @Revision: 1.0
+ * @Description:
+ */
+public class demo01 {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums1) {
+            int count = map.getOrDefault(num, 0) + 1;
+            map.put(num, count);
+        }
+        int[] intersection = new int[nums1.length];
+        int index = 0;
+        for (int num : nums2) {
+            int count = map.getOrDefault(num, 0);
+            if (count > 0) {
+                intersection[index++] = num;
+                count--;
+                if (count > 0) {
+                    map.put(num, count);
+                } else {
+                    map.remove(num);
+                }
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+
+    }
+
+    public int[] intersect2(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[Math.min(length1, length2)];
+        int index1 = 0, index2 = 0, index = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                intersection[index] = nums1[index1];
+                index1++;
+                index2++;
+                index++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums1 ={1,2,2,1} ;
+        int[] nums2 = {2,2};
+
+
+        demo01 d1 = new demo01();
+        d1.intersect(nums1,nums2);
+    }
+}
