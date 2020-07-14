682c507b89e9f48a68785241958bebbac6a301b0
feyfey <zhuxianfei1314@gmail.com>
2020-07-14 22:55:18 +0800

    DAY25

---
+++ src/main/java/Day25/demo02.java
@@ -0,0 +1,46 @@
+package Day25;
+
+import org.apache.poi.hssf.util.HSSFRegionUtil;
+
+/**
+ * @Author: ZhuXianfei
+ * @Date: 2020/7/14 22:23
+ * @Revision: 1.0
+ * @Description:
+ */
+public class demo02 {
+    //func1
+    public  String reverseStr(String s) {
+        char[] chr = s.toCharArray();
+        StringBuffer resStr = new StringBuffer();
+        for (int i = chr.length - 1; i >= 0; i--) {
+            resStr.append(chr[i]);
+        }
+        return resStr.toString();
+
+    }
+
+
+    //
+    private  String reverseString2(String str) {
+
+        if (str.length() == 1)
+            System.out.println(str);
+        else {
+            String subString1 = str.substring(0, str.length() - 1);
+            String subString2 = str.substring(str.length() - 1);
+
+            System.out.print(subString2);
+
+            reverseString2(subString1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        demo02 d2 = new demo02();
+        String s = "I am a student";
+
+        System.out.println(d2.reverseStr(s));
+    }
+}
